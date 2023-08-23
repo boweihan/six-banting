7b0a3124d3affb81eb2bc3883b60859144fce5eb
bowei <bowei.han100@gmail.com>
2023-08-22 23:22:14 -0400

    fix dates

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <h1 className="footer">Han Family Home</h1>
-      <h2 className="footer-desc">2003 - 2020</h2>
+      <h2 className="footer-desc">2002 - 2021</h2>
     </>
   );
 };
